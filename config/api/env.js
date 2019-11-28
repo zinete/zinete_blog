5588d58153ff9a715d3767c19096f335d2974bd5
zhenghui <zhenghui@zinete.com>
2019-11-28 17:53:47 +0800

    fix:修改 api 地址

--- config/api/env.js
+++ config/api/env.js
@@ -13,10 +13,10 @@ const imgBaseUrl = ''
 
 // eslint-disable-next-line no-undef
 if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:3003/'
+  baseUrl = 'http://dev.zinete.com/'
   routerMode = 'hash'
 } else {
-  baseUrl = 'http://localhost:3000/'
+  baseUrl = 'http://dev.zinete.com/'
   routerMode = 'hash'
 }
 
